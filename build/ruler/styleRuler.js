96c0157f09f60db1f70d10a1b28ffd902e9c260f
dachunchun <524112857@qq.com>
2018-08-16 11:18:58 +0800

    up 分拆styleRuler

--- build/ruler/styleRuler.js
+++ build/ruler/styleRuler.js
@@ -1,15 +1,32 @@
+const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const config = require('./../config')
 
+
+console.log(path.join(__dirname, './../../', 'src'))
+
+const cssLoader = {
+    test: /\.css$/,
+    // 因为只有node_modules里面有css，所以只针对该文件夹下文件进行编译
+    include: [path.join(__dirname, './../../', 'node_modules')],
+    use: [
+        config.extractCss ? MiniCssExtractPlugin.loader : 'style-loader',
+        'css-loader'
+    ]
+}
+
+const scssLoader = {
+    test: /\.scss$/,
+    // 只针对src下的scss进行编译
+    include: [path.join(__dirname, './../../', 'src')],
+    use: [
+        config.extractCss ? MiniCssExtractPlugin.loader : 'style-loader',
+        'css-loader',
+        'sass-loader'
+    ]
+}
+
 module.exports = [
-    {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-            // 如果为dev环境就使用css代码分离，否则使用style-loader
-            config.extractCss ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
-            'sass-loader'
-        ]
-    }
+    cssLoader, scssLoader
 ]
