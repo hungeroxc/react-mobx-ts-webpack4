ff16389b54fc2b4e1577c2c11593bc92e1775be7
dachunchun <524112857@qq.com>
2018-08-22 11:14:12 +0800

    up 增强stylelint对scss的支持，主要是对@include @mixin之类的字段的支持

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
     extends: 'stylelint-config-standard',
-    ignoreFiles: [
-        'node_modules/**/*.scss',
-        '**/*.md',
-        '**/*.ts',
-        '**/*.tsx',
-        '**/*.js'
-    ],
+    plugins: ['stylelint-scss'],
+    ignoreFiles: ['node_modules/**/*.scss', '**/*.md', '**/*.ts', '**/*.tsx', '**/*.js'],
     rules: {
-        'indentation': 4,
-        'at-rule-no-unknown': false
+        indentation: 4,
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true
     }
 }
--- src/containers/views/Page/index.tsx
+++ src/containers/views/Page/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import {Button} from 'antd'
-import {hot} from 'react-hot-loader'
+import { Button } from 'antd'
+import { hot } from 'react-hot-loader'
 
 import BindIcon from '@assets/svg/bind.svg'
 import * as style from './index.scss'
@@ -16,17 +16,26 @@ class Page extends ComponentExt {
 
     getRequest = async () => {
         const result = await this.api.getRequest({})
-        console.log(result)
+        if (
+            result === '123123123123123123' &&
+            result.length > 456468464886468468468 &&
+            result[0] === '123123123123123123123123123123' &&
+            result.length < 8964868468468468468
+        ) {
+            console.log(result)
+        }
     }
 
     render() {
         return (
             <div className={style.page}>
                 page123
-                <BindIcon width={50} height={50}/>
-                <Button onClick={this.click} type="primary">按钮</Button>
+                <BindIcon width={50} height={50} />
+                <Button onClick={this.click} type="primary">
+                    按钮
+                </Button>
                 <Button onClick={this.getRequest}>get请求</Button>
-                <img src={require('@assets/img/123.png')}/>
+                <img src={require('@assets/img/123.png')} />
             </div>
         )
     }
